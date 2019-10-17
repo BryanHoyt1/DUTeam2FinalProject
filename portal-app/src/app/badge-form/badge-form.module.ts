62937296a906a0a595de5063af238f350fc59139
Towell <AST1128@daugherty.com>
2019-10-17 16:46:24 -0500

    Can't get badge to style

--- portal-app/src/app/badge-form/badge-form.module.ts
+++ portal-app/src/app/badge-form/badge-form.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { BadgeFormComponent } from './badge-form.component';
+import { MatInputModule, MatFormFieldModule, MatButtonModule, MatSnackBarModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -9,7 +11,13 @@ import { BadgeFormComponent } from './badge-form.component';
   imports: [
     FlexLayoutModule,
     FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    RouterModule,
 
   ],
+  exports: [BadgeFormModule]
 })
 export class BadgeFormModule { }
