2ec6ffc97a2c10228fb5a6db4cac80e1bc45651d
McKim <AAM1022@daugherty.com>
2019-10-22 08:42:47 -0500

    added mail route to index.js

--- server-side/index.js
+++ server-side/index.js
@@ -23,7 +23,7 @@ app.use('/login', loginRouter);
 app.use('/home', newHireRouter);
 app.use('/extHome', extNewHireRouter);
 app.use('/badge', badgeRouter);
-//app.use('/mail', mailRouter);
+app.use('/mail', mailRouter);
 //app.use('/mail', bodyParser.json(), mailRouter);
 
 
