a4226f24d8e0fa3feba3df606002b9e4d4c2e7b7
McKim <AAM1022@daugherty.com>
2019-10-23 09:39:58 -0500

    employee put request working

--- server-side/models/newHire.js
+++ server-side/models/newHire.js
@@ -2,7 +2,7 @@ class NewHire {
 //newHireId is the primary key for 
 //TODO: figure out how to handle status (1 of 8, 4 of 8, etc)
 
-    constructor(employee_id, lastname, firstname, employee_num, personal_email, phone, birthdate, los_title, status, recruiter, startdate) {
+    constructor(employee_id, lastname, firstname, employee_num, personal_email, phone, birthdate, los_title, recruiter, startdate) {
         
         this.employee_id = employee_id;
         this.lastname = lastname;
@@ -12,7 +12,7 @@ class NewHire {
         this.phone = phone;
         this.birthdate = birthdate;
         this.los_title = los_title;
-        this.status = status;
+        //this.status = status;
         this.recruiter = recruiter;
         this.startdate = startdate;
     }
